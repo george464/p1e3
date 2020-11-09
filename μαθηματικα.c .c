d45861088ae000f33b6c578196e8d28125005fbf
γιωργος <georgebf154@gmail.com>
2020-11-09 15:42:42 +0200

    first file ever

---
+++ μαθηματικα.c .c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+[
+
+float a=10.2
+float b=13.9
+float A1=a+b;
+float A2=a-b;
+float A3=a*b;
+float A4=a/b;
+printf ("Apotelesma Prostesis: %f\n",A1)
+printf("Apotelesma Afairesis: %f\n ",A2)
+printf (Apotelesma pollaplasiasmoy:%f/n,A3)
+printf ("Apotelesma diairesis: %f\n",A4)
+
+return 0;
+]
